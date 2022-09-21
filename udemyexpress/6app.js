6ba845e6f72079950764af89a742ae1962aa8479
SHIVAM <angara8199@gmail.com>
2022-09-21 14:54:11 +0530

    second commit

---
+++ udemyexpress/6app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const adminRoutes=require('./routes/admin')
+
+const shopRoutes=require('./routes/shop')
+app.use(bodyParser.urlencoded())
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+app.listen(9000)
